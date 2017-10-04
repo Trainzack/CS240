251038e34f9713652a86cf465c13647c670d9cd7
Eli <eli@localhost.localdomain>
2017-10-03 19:35:44 -0700

    Updated gitignore and added ArrayBag file!

---
+++ Bag Interface/ArrayBag.java
@@ -0,0 +1,22 @@
+/**
+ * A class of bags implemented by fixed arrays.
+ *
+ **/
+
+public final class ArrayBag<T> implements BagInterface<T> {
+	
+	// Member Variables
+	private static final int DEFAULT_CAPACITY = 20;
+	
+	public ArrayBag(){
+		
+		this(DEFAULT_CAPACITY);
+		
+	}
+	
+	public ArrayBag(int desiredSize) {
+		
+		
+		
+	}
+}
