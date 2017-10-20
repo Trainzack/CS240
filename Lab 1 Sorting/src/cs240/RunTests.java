c617dd1dc9d4b3ccf48dae5ffe72ea1cfbb73622
Eli Zupke <eazupke@cpp.edu>
2017-10-19 19:12:08 -0700

    Added Homework 3 files and began on Lab 1

---
+++ Lab 1 Sorting/src/cs240/RunTests.java
@@ -0,0 +1,18 @@
+package cs240;
+/**
+ * 
+ * 
+ * @author eli
+ *
+ */
+public class RunTests {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
