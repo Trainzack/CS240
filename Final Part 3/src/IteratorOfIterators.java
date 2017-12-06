98f0720e7542cec8b021ab32ce404ca19ba98fd9
Eli Zupke <eazupke@cpp.edu>
2017-12-05 22:16:29 -0800

    Finalized final 3

--- Final Part 3/src/IteratorOfIterators.java
+++ Final Part 3/src/IteratorOfIterators.java
@@ -3,16 +3,21 @@ import java.util.NoSuchElementException;
 
 /**
  * 
- * 
+ * Iterates over an array of Iterators, and collates the results.
+ * Example:
+ * 	Iterating over IA and IB, each with three items, yields:
+ * 		A1 B1 A2 B2 A3 B3
+ * 	In that order
  * @author eli
- *
  *
  * @param <T> What type of objects the iterator's iterators are iterating over
  */
 public class IteratorOfIterators<T> implements Iterator<T> {
 
+	// This array contains the iterators that we will be iterating over.
 	private Iterator<T>[] its;
 	
+	// This is the index of the iterator in its that is due to be given next.
 	private int nextIndex;
 	
 	/**
--- Final Part 3/src/Test.java
+++ Final Part 3/src/Test.java
@@ -1,6 +1,16 @@
 import java.util.Iterator;
 
-
+/**
+ * This is a class designed to test Person.java and IteratorOfIterators.java
+ * 
+ * To test Person.java, it runs the two methods provided
+ * 
+ * To test IteratorOfIterators.java, it supplies that class with three separate iterators, each with the same number of elements.
+ * 	It then iterates over them, and prints out the result.
+ * 
+ * @author eli
+ *
+ */
 public class Test {
 
 	/**
@@ -17,6 +27,7 @@ public class Test {
 		System.out.println("Person Test 2");
 		System.out.println();
 		try {
+			// Test2 should end in a RuntimeException.
 			test2();
 			System.out.println("Uh-Oh! We were expecting a runtime exception!");
 		} catch (RuntimeException e) {
@@ -25,20 +36,30 @@ public class Test {
 		}
 		System.out.println();
 		System.out.println();
+		
 		System.out.println("Iterators of Iterators:");
 
+		
+		// These are the three data structures that we will iterate over.
 		VectorStack<String> stack = new VectorStack<>();
 		DoubleLinkedList<String> list = new DoubleLinkedList<>();
 		SortedDictionaryStaticArray<Integer, String> dict = new SortedDictionaryStaticArray<>(LENGTH);
 
+		// We need to fill up the data structures, so we add some labled and numbered strings to them.
 		for (int i = 0; i < LENGTH; i++) {
 			stack.push("S" + i);
 			list.add("L" + i);
+			// The key of the dictionary is an integer, so that it will sort to the right place.
 			dict.add(new Integer(i), "D" + i);
 		}
 		
-		Iterator[] its = {stack.getIterator(), list.getIterator(), dict.getValueIterator()};
-
+		// There is probably a better way to get rid of these warnings, but I am at a loss.
+		//	This is okay
+		@SuppressWarnings("rawtypes") 
+		Iterator[] its = new Iterator[] {stack.getIterator(), list.getIterator(), dict.getValueIterator()};
+		
+		//	This is okay because we just declared it earlier, so we know they will match.
+		@SuppressWarnings("unchecked")
 		IteratorOfIterators<String> t = new IteratorOfIterators<>(its);
 
 		while (t.hasNext()) {
